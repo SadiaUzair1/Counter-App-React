05a20409164b0bf9c111631b0a7365cdc4d89769
Sadia Uzair <sadia.uzair@devsinc.com>
2021-10-06 13:10:03 +0500

    added onClick Functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 class Counter extends React.Component {
     state = {
-        tags: ['tag1', 'tag2', 'tag3']
+        count: 0
     }
 
-    renderTags(){
-        if(this.state.tags.length === 0) return null;
-        return <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>
+    handleIncrement(){
+        console.log('Increment Clicked');
     }
-
     render() { 
         return ( <React.Fragment>
-            {this.state.tags.length === 0 && "No Tags"}
-            {this.renderTags()}
+            <span className="btn btn-primary m-2">{this.formatCounter()}</span>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
 
-
+    formatCounter(){
+        const {count} = this.state;
+        return count === 0? <h1>Zero</h1> :count;
+    }
 }
  
 export default Counter;

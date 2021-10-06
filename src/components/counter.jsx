a24f6eed46bf21d619690a075c1305cfc4226fb2
Sadia Uzair <sadia.uzair@devsinc.com>
2021-10-06 13:02:19 +0500

    did conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 class Counter extends React.Component {
     state = {
-        count: 0,
-        imageURL: 'https://picsum.photos/500',
         tags: ['tag1', 'tag2', 'tag3']
     }
+
+    renderTags(){
+        if(this.state.tags.length === 0) return null;
+        return <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>
+    }
+
     render() { 
         return ( <React.Fragment>
-            <img src={this.state.imageURL} alt=""/>
-            <span className="btn btn-primary m-2">{this.formatCounter()}</span>
-            <button style={{fontSize:40}} className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map(tag =><li key={tag}>{tag}</li>)}
-            </ul>
+            {this.state.tags.length === 0 && "No Tags"}
+            {this.renderTags()}
             </React.Fragment>
         );
     }
 
-    formatCounter(){
-        const {count} = this.state;
-        return count === 0? <h1>Zero</h1> :count;
-    }
+
 }
  
 export default Counter;
